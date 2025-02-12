5836dbeea6ed939c08298a64a6296d4cfcbe2509
Le Zhang <you@example.com>
2025-02-12 15:16:14 -0500

    move copy/paste keys to the left of space

--- keyboards/converter/usb_usb/keymaps/lzhang10.magic/keymap.c
+++ keyboards/converter/usb_usb/keymaps/lzhang10.magic/keymap.c
@@ -26,6 +26,9 @@
 #define O_LSFT OSM(MOD_LSFT)
 #define O_RSFT OSM(MOD_RSFT)
 
+#define CPY_CUT TD(TD_CPY_CUT)
+#define PASTE C(KC_V)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT_magic(
@@ -34,7 +37,7 @@ KC_GRV,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  
 KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,
 KC_ESC,   CMD_A, ALT_S, SFT_D, CTL_F, KC_G,  KC_H,  CTL_J, SFT_K, ALT_L, CMD_SC, KC_QUOT,      KC_ENT,
 O_LSFT,   KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH,  O_RSFT,       KC_UP,
-KC_LCTL, KC_LALT, KC_LGUI,             SPACE_FN,                C(KC_V), TD(TD_CPY_CUT),    KC_LEFT, KC_DOWN, KC_RGHT
+KC_LCTL, CPY_CUT,PASTE,           SPACE_FN,                KC_LGUI, KC_LALT,               KC_LEFT, KC_DOWN, KC_RGHT
 ),
     [NAV] = LAYOUT_magic(
 TRNS, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, STR_A, TRNS,
